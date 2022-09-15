26bafd703d12a8e77c3b2b44e1f17b1ac2ffebfd
Lux <anguyen780@gmail.com>
2022-09-15 14:44:16 -0500

    added checked inputs to check boxes, still trying to work through it

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,4 @@
+//hides check box for icon inputs 
 $(document).ready(function(){
     $('#happyCheck').hide()
     $('#sadCheck').hide()
@@ -11,19 +12,30 @@ $(document).ready(function(){
 
 function goSearch() {
     //will be built from screen inputs
-    $('#happy') = 'g.115,g.71,g.75'
-    $('#sad') = 'g.33,g.407,g.299,g.21,g.453,g.438'
-    $('#angry') = 'g.394,g.146,g.33,g.5'
-    $('#loving') = 'g.115,g.194,g.75,g.69'
-    $('#silly') = 'g.156,g.470'
-    $('#cheerful') = 'g.115,g.71,g.4,g.75,g.446'
-    $('#motivated') = 'g.5,g.146,g.71,g.246,g.394'
-    $('#sleepy') = 'g.299,g.21,g.453,g.438,g.69'
-    
     var searchTerms = "g.1051,g.1050"
     document.location.replace("./results.html?="+searchTerms)
 }
 
 
+function checkInput (){
+// pushes checked inputs into array 
+var checkedInput = $('input:checked');
+var checked = [];
+
+$.each(checkedInput, function(){
+    checked.push($(this).val());
+});
+
+    $('#happy-check').val('g.115,g.71,g.75');
+    $('#sad').val('g.33,g.407,g.299,g.21,g.453,g.438') ;
+    $('#angry').val('g.394,g.146,g.33,g.5');
+    $('#loving').val('g.115,g.194,g.75,g.69');
+    $('#silly').val('g.156,g.470');
+    $('#cheerful').val('g.115,g.71,g.4,g.75,g.446');
+    $('#motivated').val('g.5,g.146,g.71,g.246,g.394');
+    $('#sleepy').val('g.299,g.21,g.453,g.438,g.69');
+console.log(checked);
+}
+// $('button').on('click', checkInput)
 
 $("button").on("click", goSearch)
