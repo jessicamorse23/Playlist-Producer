9b68a602af2f720551e7e0c365d81a2e369ec718
Lux <anguyen780@gmail.com>
2022-09-16 11:43:14 -0500

    finished linking up emojis to genre ids on both index and results html

--- assets/script/results.js
+++ assets/script/results.js
@@ -22,6 +22,43 @@ var artistDetail = []
 
 var listIndex = 0;
 
+//hides check box for icon inputs 
+$(document).ready(function () {
+    $('#happyCheck').hide()
+    $('#sadCheck').hide()
+    $('#angryCheck').hide()
+    $('#lovingCheck').hide()
+    $('#sillyCheck').hide()
+    $('#cheerfulCheck').hide()
+    $('#motivatedCheck').hide()
+    $('#sleepyCheck').hide()
+})
+
+// sets values of moods to the genres
+$('#happyCheck').val('g.115,g.71');
+$('#sadCheck').val('g.33,g.438');
+$('#angryCheck').val('g.394,g.146');
+$('#lovingCheck').val('g.115,g.194');
+$('#sillyCheck').val('g.156,g.470');
+$('#cheerfulCheck').val('g.115,g.4');
+$('#motivatedCheck').val('g.5,g.71');
+$('#sleepyCheck').val('g.21,g.438');
+
+$('#submit-btn').on('click', goSearch)
+
+function goSearch() {
+    //pushes checked inputs into array
+    var checkedInput = $('input:checked');
+    var checked = [];
+
+    $.each(checkedInput, function () {
+        checked.push($(this).val());
+    });
+    var searchTerms = checked
+    document.location.replace("./results.html?=" + searchTerms)
+    getPlaylists();
+}
+
 function displayScreen() {
 
     for(card = 1; card <= 4; card++) {
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,5 @@
 //hides check box for icon inputs 
-$(document).ready(function(){
+$(document).ready(function () {
     $('#happyCheck').hide()
     $('#sadCheck').hide()
     $('#angryCheck').hide()
@@ -8,68 +8,28 @@ $(document).ready(function(){
     $('#cheerfulCheck').hide()
     $('#motivatedCheck').hide()
     $('#sleepyCheck').hide()
-  })
+})
 
-var apiKey ="NzE2OGZiMzEtYjAyMi00ZjM5LWI2ZGEtNzUwZWI3YmExMWQ1"
-var genreUrl = `http://api.napster.com/v2.2/genres?apikey=${apiKey}`
-
-fetch(genreUrl)
-    .then(function(response){
-        return response.json()
-    })
-    .then (function(results){
-        // gets genre ids from api 
-        var popGenre = results.genres[0].id
-        var elGenre = results.genres[7].id
-        var gospelGenre = results.genres[14].id
-        var altGenre = results.genres[2].id
-        var countryGenre = results.genres[5].id
-        var jazzGenre = results.genres[6].id
-        var classicalGenre = results.genres[11].id
-        var newAgeGenre = results.genres[13].id
-        var bluesGenre = results.genres[15].id
-        var metalGenre = results.genres[21].id
-        var rapGenre = results.genres[3].id
-        var rockGenre = results.genres[1].id
-        var rnbGenre = results.genres[4].id
-        var vocalGenre = results.genres[17].id
-        var comedyGenre = results.genres[20].id
-        var childrenGenre = results.genres[19].id
-        var oldiesGenre = results.genres[12].id
-        var folkGenre = results.genres[16].id
-        var soundTracksGenre = results.genres[18].id
-
-        // sets values of moods to the genres
-        $('#happyCheck').val(`${popGenre},${elGenre},${gospelGenre}`);
-        $('#sadCheck').val(`${altGenre},${countryGenre},${jazzGenre},${classicalGenre},${newAgeGenre},${bluesGenre}`);
-        $('#angryCheck').val(`${metalGenre},${rapGenre},${altGenre},${rockGenre}`);
-        $('#lovingCheck').val(`${popGenre},${rnbGenre},${gospelGenre},${vocalGenre}`);
-        $('#sillyCheck').val(`${comedyGenre},${childrenGenre}`);
-        $('#cheerfulCheck').val(`${popGenre},${elGenre},${oldiesGenre},${gospelGenre},${folkGenre}`);
-        $('#motivatedCheck').val(`${rockGenre},${rapGenre},${elGenre},${soundTracksGenre},${metalGenre}`);
-        $('#sleepyCheck').val(`${jazzGenre},${classicalGenre},${newAgeGenre},${bluesGenre},${vocalGenre}`);
-    })
-
-
+// sets values of moods to the genres
+$('#happyCheck').val('g.115,g.71');
+$('#sadCheck').val('g.33,g.438');
+$('#angryCheck').val('g.394,g.146');
+$('#lovingCheck').val('g.115,g.194');
+$('#sillyCheck').val('g.156,g.470');
+$('#cheerfulCheck').val('g.115,g.4');
+$('#motivatedCheck').val('g.5,g.71');
+$('#sleepyCheck').val('g.21,g.438');
 
 function goSearch() {
-    //will be built from screen inputs
-    var searchTerms = "g.1051,g.1050"
-    document.location.replace("./results.html?="+searchTerms)
+    //pushes checked inputs into array
+    var checkedInput = $('input:checked');
+    var checked = [];
+
+    $.each(checkedInput, function () {
+        checked.push($(this).val());
+    });
+    var searchTerms = checked
+    document.location.replace("./results.html?=" + searchTerms)
 }
-
-
-function checkInput (){
-// pushes checked inputs into array 
-var checkedInput = $('input:checked');
-var checked = [];
-
-$.each(checkedInput, function(){
-    checked.push($(this).val());
-    console.log(checked);
-});
-
-}
-// $('button').on('click', checkInput)
 
 $("button").on("click", goSearch)
